34e2c6981614114d6095dddca9415039cbca09ed
yomaic2015 <yomaic2015@gmail.com>
2023-03-06 02:11:33 -0500

    initial commit

---
+++ createinstance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "AKIA4R3Y5WLQUNBALG5T"
+  secret_key = "MY_SECRET"
+  region     = "ca-central-1"
+}
+
+resources "aws_instance" "MyFirstInstance" {
+  ami            = "ami-003be126fdbb92daa1"
+  instance_type  = "t2.micro"
+}    
