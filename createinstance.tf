c37681aea1efeeadf79226486c359510c6972bd2
yomaic2015 <yomaic2015@gmail.com>
2023-03-13 05:30:57 -0400

    updated commit

--- createinstance.tf
+++ createinstance.tf
@@ -2,7 +2,6 @@ provider "aws" {
   access_key = "AKIA4R3Y5WLQUNBALG5T"
   secret_key = "MY_SECRET"
   region     = "ca-central-1"
-  provisioner: "aws"
 }
 
 resources "aws_instance" "MyFirstInstance" {
